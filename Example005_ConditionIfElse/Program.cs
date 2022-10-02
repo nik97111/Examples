b3c88e81fa952b9d09e92c0274b3fe5a1b92354e
nik <nik@MacBook-Air-nik.local>
2022-10-02 23:21:19 +0500

    Example 005 Condition If Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console. WriteLine("Введите имя пользователя");
+string usernaame = Console.ReadLine();
+
+if (usernaame.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! Это же Маша!");
+}
+else 
+{
+    Console.Write("Привет ,");
+    Console.WriteLine(usernaame);
+}
