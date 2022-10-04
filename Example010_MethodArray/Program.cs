e82747a89a2fcb898bc7e1e12b7e3f08761b6574
nik <nik@MacBook-Air-nik.local>
2022-10-04 19:37:37 +0500

    Example 010 MethodArray

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,32 @@
+﻿// int[] array = {1, 12, 31, 4, 15, 16, 17, 18};
+
+// int n = array.Length;
+// int find = 18;
+
+// int index = 0;
+
+// while (index < n) 
+// {
+//     if (array[index] == find) 
+//     {
+//         Console.WriteLine(index);
+//     }
+//     index +=1;
+// }
+
+int[] array = {1, 12, 31, 4, 18, 15, 16, 17, 18};
+
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+
+while (index < n) 
+{
+    if (array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index +=1;
+}
